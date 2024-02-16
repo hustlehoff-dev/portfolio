429a9fb91ec15d4d5492fc017c233c64911f18b0
hustlehoff-dev <148590992+hustlehoff-dev@users.noreply.github.com>
2024-02-17 00:30:31 +0100

    Components update

---
+++ src/components/Contact.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <div className="contact-wrapper">
+        <div className="contact-contents">
+          <h3>Contact me</h3>
+          <h2>
+            Let's talk about a project, collaboration on any idea you may have
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import Home from "./Home";
 import Aboutme from "./About-me";
 import Techstack from "./Techstack";
 import Projects from "./Projects";
+import Contact from "./Contact";
 
 const Main = () => {
   return (
@@ -10,16 +11,7 @@ const Main = () => {
       <Aboutme />
       <Techstack />
       <Projects />
-      <section id="contact">
-        <div className="contact-wrapper">
-          <div className="contact-contents">
-            <h3>Contact me</h3>
-            <h2>
-              Let's talk about a project, collaboration on any idea you may have
-            </h2>
-          </div>
-        </div>
-      </section>
+      <Contact />
     </main>
   );
 };
